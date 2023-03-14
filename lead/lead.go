2c8c137bf31c188022c96a5dcbd955c3354190e3
Mustafa-ans <112391427+Mustafa-ans@users.noreply.github.com>
2023-03-14 18:38:22 +0530

    refactor

--- lead/lead.go
+++ lead/lead.go
@@ -2,57 +2,56 @@ package lead
 
 import (
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	 _ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/gofiber/fiber"
 	"github.com/Mustafa-ans/go-fibre/database"
 )
 
-type Lead struct{
+type Lead struct {
 	gorm.Model
-	Name string 	`json:"name"`
-	Company string 	`json:"company"`
-	Email string 	`json:"email"`
-	Phone int 		`json:"phone"`
-	 
+	Name    string `json:"name"`
+	Company string `json:"company"`
+	Email   string `json:"email"`
+	Phone   int    `json:"phone"`
 }
 
-func GetLeads(c *fiber.Ctx){
-	db:= database.DBConn
+func GetLeads(c *fiber.Ctx) {
+	db := database.DBConn
 	var leads []Lead
 	db.Find(&leads)
 	c.JSON(leads)
-
 }
 
-func GetLead(c *fiber.Ctx){
+func GetLead(c *fiber.Ctx) {
 	id := c.Params("id")
 	db := database.DBConn
 	var lead Lead
-	db.Find(&lead,id)
+	db.Find(&lead, id)
 	c.JSON(lead)
 }
 
-func NewLead(c *fiber.Ctx){
-	db:= database.DBConn
+func NewLead(c *fiber.Ctx) {
+	db := database.DBConn
 	lead := new(Lead)
-	if err:= c.BodyParser(lead); err!= nil{
+	if err := c.BodyParser(lead); err != nil {
 		c.Status(503).Send(err)
 		return
 	}
 	db.Create(&lead)
 	c.JSON(lead)
+
 }
 
-func DeleteLead(c *fiber.Ctx){
+func DeleteLead(c *fiber.Ctx) {
 	id := c.Params("id")
 	db := database.DBConn
-	var lead Lead // Lead is the struct created above and the lead is the name of the variable
-	
-	db.First(&lead,id)
-	if lead.Name == ""{
-		c.Status(500).Send("No lead found with the given id")
+
+	var lead Lead
+	db.First(&lead, id)
+	if lead.Name == "" {
+		c.Status(500).Send("No lead found with ID")
 		return
 	}
 	db.Delete(&lead)
-	c.Send("Lead deleted")
+	c.Send("Lead successfully Deleted")
 }
--- main.go
+++ main.go
@@ -2,30 +2,29 @@ package main
 
 import (
 	"fmt"
-	"github.com/Mustafa-ans/go-fibre/lead"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/gofiber/fiber"
 	"github.com/Mustafa-ans/go-fibre/database"
+	"github.com/Mustafa-ans/go-fibre/lead"
 )
 
-func setupRoutes(app *fiber.App){
-	app.Get("/api/v1/lead",lead.GetLeads)
-	app.Get("/api/v1/lead/:id",lead.GetLead)
-	app.Post("/api/v1/lead",lead.Newlead)
-	app.Delete("/api/v1/lead:id",lead.DeleteLead)
-
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead/:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead/:id", lead.DeleteLead)
 }
 
-func initDatabase(){
+func initDatabase() {
 	var err error
-	database.DBConn, err = gorm.Open("sqlite3","leads.db")
-	if err != nil{
-		panic("failed to connect database :(")
-		fmt.Println("Connection opened to database :)")
-		database.DBConn.AutoMigrate(&lead.Lead{})
-		fmt.Println("Database migrated :D")
+	database.DBConn, err = gorm.Open("sqlite3", "leads.db")
+	if err != nil {
+		panic("failed to connect database")
 	}
+	fmt.Println("Connection opened to database")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database Migrated")
 }
 
 func main() {
@@ -34,6 +33,4 @@ func main() {
 	setupRoutes(app)
 	app.Listen(3000)
 	defer database.DBConn.Close()
-	
-	
 }
