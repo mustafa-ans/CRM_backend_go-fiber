3b64acb521db5007933a1469b4980944655152f5
Mustafa-ans <112391427+Mustafa-ans@users.noreply.github.com>
2023-03-14 16:39:14 +0530

    lead.go complete

--- lead/lead.go
+++ lead/lead.go
@@ -1,12 +1,58 @@
 package lead
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/gofiber/fiber"
+	"github.com/Mustafa-ans/go-fibre/database"
+)
 
 type Lead struct{
 	gorm.Model
-	Name string
-	Company string
-	Email string
-	Phone int
+	Name string 	`json:"name"`
+	Company string 	`json:"company"`
+	Email string 	`json:"email"`
+	Phone int 		`json:"phone"`
 	 
+}
+
+func GetLeads(c *fiber.Ctx){
+	db:= database.DBConn
+	var leads []Lead
+	db.Find(&leads)
+	c.JSON(leads)
+
+}
+
+func GetLead(c *fiber.Ctx){
+	id := c.Params("id")
+	db := database.DBConn
+	var lead Lead
+	db.Find(&lead,id)
+	c.JSON(lead)
+}
+
+func NewLead(c *fiber.Ctx){
+	db:= database.DBConn
+	lead := new(Lead)
+	if err:= c.BodyParser(lead); err!= nil{
+		c.Status(503).Send(err)
+		return
+	}
+	db.Create(&lead)
+	c.JSON(lead)
+}
+
+func DeleteLead(c *fiber.Ctx){
+	id := c.Params("id")
+	db := database.DBConn
+	var lead Lead // Lead is the struct created above and the lead is the name of the variable
+	
+	db.First(&lead,id)
+	if lead.Name == ""{
+		c.Status(500).Send("No lead found with the given id")
+		return
+	}
+	db.Delete(&lead)
+	c.Send("Lead deleted")
 }
